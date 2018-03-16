8053e78728786e5a11433388cf543495b263008c
GBTC440005ur <EEnamor1@mongtomerycollege.edu>
2018-03-16 14:26:33 -0400

    Pushing changes made to the home controller

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -29,8 +29,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-       http.authorizeRequests().antMatchers( "./css/**","./js/**", "/","/h2-console/**", "/register").permitAll().anyRequest().authenticated()
-               .and().formLogin().loginPage("/login").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+       http.authorizeRequests().antMatchers( "./css/**","./js/**", "/","/h2-console/**", "/register")
+               .permitAll().anyRequest().authenticated()
+               .and()
+               .formLogin().loginPage("/login").permitAll()
+               .and()
+               .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                .logoutSuccessUrl("/").permitAll().and().httpBasic();
 
         http.csrf().disable();
